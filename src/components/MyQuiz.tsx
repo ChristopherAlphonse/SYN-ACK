a7d24c18445a2b2aa247e2ebf30654eb49b2d4af
christopheralphonse <christopheralphonse96@gmail.com>
2024-04-25 17:48:10 -0400

    fix bug with data deleting when user restart

--- src/components/MyQuiz.tsx
+++ src/components/MyQuiz.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import "react-toastify/dist/ReactToastify.css";
-
 import {
     Box,
     Button,
@@ -13,22 +11,35 @@ import {
     RadioGroup,
     Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { RiFolderUploadLine } from "react-icons/ri";
 import { getMockQuizData } from "./getMockQuizData";
 import { toast } from "react-toastify";
 
 const MyQuiz: React.FC = () => {
-    const [questions, setQuestions] = useState<QuizQuestion[]>(
-        getMockQuizData(),
+    const [initialQuestions, setInitialQuestions] = useState(
+        JSON.parse(localStorage.getItem("quizData") || "null") ||
+            getMockQuizData(),
     );
+    const [questions, setQuestions] = useState(initialQuestions || []);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedOption, setSelectedOption] = useState("");
     const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
     const [wrongAnswersCount, setWrongAnswersCount] = useState(0);
-
     const [showStats, setShowStats] = useState(false);
+    const [loadedQuestions, setLoadedQuestions] = useState([]);
+
+    useEffect(() => {
+        localStorage.setItem("quizData", JSON.stringify(initialQuestions));
+    }, [initialQuestions]);
+
+    useEffect(() => {
+        if (loadedQuestions.length > 0) {
+            setQuestions(loadedQuestions);
+        }
+    }, [loadedQuestions]);
+
     const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
@@ -36,10 +47,11 @@ const MyQuiz: React.FC = () => {
             reader.onload = (event) => {
                 try {
                     const jsonData = JSON.parse(event.target?.result as string);
-                    setQuestions(jsonData);
+                    setLoadedQuestions(jsonData);
                     setCurrentQuestionIndex(0);
                     setCorrectAnswersCount(0);
                     setSelectedOption("");
+                    setShowStats(false);
                 } catch (error) {
                     console.error("Error parsing JSON file:", error);
                     toast.error(
@@ -52,11 +64,13 @@ const MyQuiz: React.FC = () => {
     };
 
     const handleRestart = () => {
-        setQuestions(getMockQuizData());
+        if (loadedQuestions.length > 0 || !initialQuestions) {
+            setQuestions(loadedQuestions);
+        }
         setCurrentQuestionIndex(0);
         setSelectedOption("");
         setCorrectAnswersCount(0);
-        setWrongAnswersCount(0); // Reset wrong answers count
+        setWrongAnswersCount(0);
         setShowStats(false);
     };
 
@@ -67,7 +81,8 @@ const MyQuiz: React.FC = () => {
     };
 
     const handleSubmit = () => {
-        if (selectedOption === questions[currentQuestionIndex].correctAnswer) {
+        const currentQuestion = questions[currentQuestionIndex];
+        if (selectedOption === currentQuestion.correctAnswer) {
             setCorrectAnswersCount((prevCount) => prevCount + 1);
             toast.success("Correct answer!");
         } else {
@@ -107,6 +122,9 @@ const MyQuiz: React.FC = () => {
         }
     };
 
+    const currentQuestions =
+        loadedQuestions.length > 0 ? loadedQuestions : questions;
+
     return (
         <div className="px-4">
             <h1 className="text-5xl md:text-9xl capitalize text-gray-600 leading-widest text-center pb-10 md:pb-20 tracking-widest">
@@ -115,7 +133,7 @@ const MyQuiz: React.FC = () => {
             <div className="pb-3">
                 <label
                     htmlFor="uploadFile1"
-                    className="flex items-center justify-center text-base outline-none w-max cursor-pointer mx-auto font-[sans-serif] px-4 md:px-9 py-1.5 border border-gay-600 text-white rounded-md shadow-md hover:bg-gray-900 transition duration-300 ease-in-out gap-3"
+                    className="flex items-center justify-center text-base outline-none w-max cursor-pointer mx-auto font-[sans-serif] px-4 md:px-9 py-1.5 border border-gray-600 text-white rounded-md shadow-md hover:bg-gray-900 transition duration-300 ease-in-out gap-3"
                 >
                     JSON {"{ }"}
                     <input
@@ -223,7 +241,7 @@ const MyQuiz: React.FC = () => {
                     <div className="mb-9">
                         <button
                             onClick={handleRestart}
-                            className="px-9 py-1.5 border border-gay-600 text-white rounded-md shadow-md hover:bg-gray-900 transition duration-300 ease-in-out"
+                            className="px-9 py-1.5 border border-gray-600 text-white rounded-md shadow-md hover:bg-gray-900 transition duration-300 ease-in-out"
                         >
                             Restart
                         </button>
